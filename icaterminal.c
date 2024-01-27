6f7d1fd2973524509e47958a8fe5934fdf1fe5be
Icarogamer2441 <148513434+Icarogamer2441@users.noreply.github.com>
2024-01-27 12:11:51 -0300

    Update icaterminal.c

--- icaterminal.c
+++ icaterminal.c
@@ -85,7 +85,7 @@ int main() {
             scanf("%s", command);
             
             if(strcmp(command, "version") == 0){
-                printf("icaterminal v2.4\n");
+                printf("icaterminal v2.5\n");
             }
             if(strcmp(command, "exit") == 0){
                 running = 0;
@@ -124,6 +124,7 @@ int main() {
 				printf("toretori\n");
 				printf("addedthings\n");
 				printf("kill\n");
+				printf("editicaterminal\n");
             }
             if(strcmp(command, "open") == 0){
                 char url[200];
@@ -274,7 +275,7 @@ int main() {
                             printf("Creator: jose icaro\n");
                         }
                         if(strcmp(question,"version") == 0){
-                            printf("Version: 2.4\n");
+                            printf("Version: 2.5\n");
                         }
                         if(strcmp(question,"exit") == 0){
                             runapp = 0;
@@ -315,36 +316,38 @@ int main() {
                 printf("github repo: https://github.com/Icarogamer2441/icaterminal\n");
                 printf("remember! you need to know how to code in the programming language C!\n");
             }
-	        if(strcmp(command,"changename") == 0){
-		    printf("what's your new username: ");
-	            scanf("%s",username);
+	    if(strcmp(command,"changename") == 0){
+		printf("what's your new username: ");
+	        scanf("%s",username);
 
-		    printf("name sucefully set!");
-		    fprintf(arq,"new name!");
-	        }
-			if(strcmp(command,"imensy") == 0){
-			  printf("imensy is an brazilian streamer, he (or she) is lgbtq+\n");
-			  printf("imensy twitch link: https://www.twitch.tv/imensy_\n");
-			}
-			if(strcmp(command,"toretori") == 0){
-				printf("toretori is an brazilian streamer\n");
-				printf("toretori twitch link: https://www.twitch.tv/toretori\n");
-			}
-			if(strcmp(command,"addedthings") == 0){
-				printf("in this update has been added:\n");
-				printf("command: addedthings\n");
-				printf("command: kill\n");
-			}
-			if(strcmp(command,"kill") == 0){
-				system("clear") || system("cls");
-				printf("@@@@@@@@@@@@@@@@@@@@@@@BBBBBBBBBBBBBBBB@@@@@@@@@@@@@@@@@@@@@@@\n");
-				printf("@@@@@@@@@@@@@@@@@#GBBGG5YYYYYYYYYYYYYY5BBBBG#@@@@@@@@@@@@@@@@@\n");
-				printf("@@@@@@@@@@@@@#GGG5YYYPPYYYYYYYYYYYYYYYYYYYYY5GGG#@@@@@@@@@@@@@\n");
-				printf("@@@@@@@@@@&#G5YYYYYYYGB5YPGGGGGGGYYYYYYGPYYYYYYY5G#&@@@@@@@@@@\n");
-				printf("@@@@@@@@&#P5YYYYYYYYYPG5Y&@BBBBBBYYY5GGBBGPYYYYYYY5P#&@@@@@@@@\n");
-				printf("@@@@@@&#P5YYYYYYYYYYY#@PY&@5YYYYYYYY5@@B#@#JYYYYYYYY5P#&@@@@@@\n");
-				printf("@@@@##55YYYYYYYYYYYYY#@PY&@5YYYYYY5#BPPGGPG#GYYYYYYYYY55##@@@@\n@@@@BGYYYYYYYYYYYYYYY#@PY&@######Y5@&YYYYYP@BYYYYYYYYYYYGB@@@@\n@@#B55YYYYYYYYYYYYYYYY5YY55555555YY55YYYYYY5YYYYYYYYYYYY55B#@@\n@@#BYYYYYYYJ?????????77??77777777??77??????7????????JYYYYYB#@@\n@&BGYYYYYYYP@&&#G#@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@PYYYYYGB&@\n#BYYYYYYYYYG@@@5^JG&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYB#\nBBYYYYYYYYYG@@@@@5!Y5#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYBB\nBBYYYYYYYYYG@@@@@BY?7B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYBBn\nBBYYYYYYYYYG@@@G???B@@@5??????????Y@@@@@@@@@@@@@@@@@PYYYYYYYBB\nBBYYYYYYYYYG@@@GJB@@@@@PYYYYYYYYYY5@@@@@@@@@@@@@@@@@PYYYYYYYBB\nBBYYYYYYYYYG@@@GJB@@@@@PYYYYYYYYYY5@@@@@@@@@@@@@@@@@PYYYYYYYBB\nBBYYYYYYYYYG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYBB\n#BYYYYYYYYYG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYB#\n@&BGYYYYYYYY55PPP5555555555PPPP555555555555555555555YYYYYYGB&@\n@@#BYYYYYYYYYP&##G5YYYYYYY5####5YYYYYYYYYYYYYYYYYYYYYYYYYYB#@@\n@@#B55YYYYYYYYY5PB&PYYYYY#@5YY5&&YYYYYYYYYYYYYYYYYYYYYYY55B#@@\n@@@@BGYYYYYYY5PG#PYYYYYYY&@5YYY@@YYYYYYYYYYYYYYYYYYYYYYYGB@@@@\n@@@@##55YYYYYG@#PGG5YPG5YB#GGGG#BYYYYYYYYYYYYYYYYYYYYY55##@@@@\n@@@@@@&#P5YYYPBBBBBPYGB5YYYGBBBYYYYYYYYYYYYYYYYYYYYY5P#&@@@@@@\n@@@@@@@@&#P5YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY5P#&@@@@@@@@\n@@@@@@@@@@&#G5YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY5G#&@@@@@@@@@@\n@@@@@@@@@@@@@#GGG5YYYYYYYYYYYYYYYYYYYYYYYYYY5GGG#@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@#GBBBB5YYYYYYYYYYYYYY5BBBBG#@@@@@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@BBBBBBBBBBBBBBBB@@@@@@@@@@@@@@@@@@@@@@@\n");
-			}
+		printf("name sucefully set!");
+		fprintf(arq,"new name!");
+	    }
+	    if(strcmp(command,"imensy") == 0){
+		printf("imensy is an brazilian streamer, he (or she) is lgbtq+\n");
+	        printf("imensy twitch link: https://www.twitch.tv/imensy_\n");
+	    }
+	    if(strcmp(command,"toretori") == 0){
+		printf("toretori is an brazilian streamer\n");
+		printf("toretori twitch link: https://www.twitch.tv/toretori\n");
+	    }
+	    if(strcmp(command,"addedthings") == 0){
+		printf("in this update has been added:\n");
+		printf("command: editicaterminal\n");
+	    }
+	    if(strcmp(command,"kill") == 0){
+		system("clear") || system("cls");
+		printf("@@@@@@@@@@@@@@@@@@@@@@@BBBBBBBBBBBBBBBB@@@@@@@@@@@@@@@@@@@@@@@\n");
+		printf("@@@@@@@@@@@@@@@@@#GBBGG5YYYYYYYYYYYYYY5BBBBG#@@@@@@@@@@@@@@@@@\n");
+		printf("@@@@@@@@@@@@@#GGG5YYYPPYYYYYYYYYYYYYYYYYYYYY5GGG#@@@@@@@@@@@@@\n");
+		printf("@@@@@@@@@@&#G5YYYYYYYGB5YPGGGGGGGYYYYYYGPYYYYYYY5G#&@@@@@@@@@@\n");
+		printf("@@@@@@@@&#P5YYYYYYYYYPG5Y&@BBBBBBYYY5GGBBGPYYYYYYY5P#&@@@@@@@@\n");
+		printf("@@@@@@&#P5YYYYYYYYYYY#@PY&@5YYYYYYYY5@@B#@#JYYYYYYYY5P#&@@@@@@\n");
+		printf("@@@@##55YYYYYYYYYYYYY#@PY&@5YYYYYY5#BPPGGPG#GYYYYYYYYY55##@@@@\n@@@@BGYYYYYYYYYYYYYYY#@PY&@######Y5@&YYYYYP@BYYYYYYYYYYYGB@@@@\n@@#B55YYYYYYYYYYYYYYYY5YY55555555YY55YYYYYY5YYYYYYYYYYYY55B#@@\n@@#BYYYYYYYJ?????????77??77777777??77??????7????????JYYYYYB#@@\n@&BGYYYYYYYP@&&#G#@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@PYYYYYGB&@\n#BYYYYYYYYYG@@@5^JG&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYB#\nBBYYYYYYYYYG@@@@@5!Y5#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYBB\nBBYYYYYYYYYG@@@@@BY?7B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYBBn\nBBYYYYYYYYYG@@@G???B@@@5??????????Y@@@@@@@@@@@@@@@@@PYYYYYYYBB\nBBYYYYYYYYYG@@@GJB@@@@@PYYYYYYYYYY5@@@@@@@@@@@@@@@@@PYYYYYYYBB\nBBYYYYYYYYYG@@@GJB@@@@@PYYYYYYYYYY5@@@@@@@@@@@@@@@@@PYYYYYYYBB\nBBYYYYYYYYYG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYBB\n#BYYYYYYYYYG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@PYYYYYYYB#\n@&BGYYYYYYYY55PPP5555555555PPPP555555555555555555555YYYYYYGB&@\n@@#BYYYYYYYYYP&##G5YYYYYYY5####5YYYYYYYYYYYYYYYYYYYYYYYYYYB#@@\n@@#B55YYYYYYYYY5PB&PYYYYY#@5YY5&&YYYYYYYYYYYYYYYYYYYYYYY55B#@@\n@@@@BGYYYYYYY5PG#PYYYYYYY&@5YYY@@YYYYYYYYYYYYYYYYYYYYYYYGB@@@@\n@@@@##55YYYYYG@#PGG5YPG5YB#GGGG#BYYYYYYYYYYYYYYYYYYYYY55##@@@@\n@@@@@@&#P5YYYPBBBBBPYGB5YYYGBBBYYYYYYYYYYYYYYYYYYYYY5P#&@@@@@@\n@@@@@@@@&#P5YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY5P#&@@@@@@@@\n@@@@@@@@@@&#G5YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY5G#&@@@@@@@@@@\n@@@@@@@@@@@@@#GGG5YYYYYYYYYYYYYYYYYYYYYYYYYY5GGG#@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@#GBBBB5YYYYYYYYYYYYYY5BBBBG#@@@@@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@BBBBBBBBBBBBBBBB@@@@@@@@@@@@@@@@@@@@@@@\n");
+	    }
+	    if(strcmp(command,"editicaterminal") == 0){
+		system("vim icaterminal.c") || system("code .") || system("emacs icaterminal.c") || system("nano icaterminal.c");
+	    }
         } else {
             if(tamanhoAtual > limite){
                 printf("Storage limit reached. Cannot perform more operations.\n");
